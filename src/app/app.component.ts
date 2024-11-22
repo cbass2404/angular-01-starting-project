3b69e9ed58f7bcfe6541a079b417f1c6040d3e76
Cory Bass <cory@Corys-MacBook-Pro.local>
2024-11-22 16:16:01 -0600

    feature: continued with course leaving off for the day at the stage of adding new tasks to the todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header.component";
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './fixtures/dummy-users';
+import { type User } from './user/user.model';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TasksComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  public users: Array<User> = DUMMY_USERS;
+  public selectedUser?: User;
+
+  public onSelectUser(id: string) {
+    this.selectedUser = this.users.find((user) => user.id === id);
+  }
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { type User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) public user!: User;
+  @Input({ required: true }) public selected!: boolean;
+
+  @Output() public select = new EventEmitter<string>();
+
+  public get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
